564a59ea70c914dd909327aa8ddfb0c68f021e39
pradeeprai1990 <pradeep.9997@gmail.com>
2025-01-03 18:17:03 +0530

    Chanegs

---
+++ React Js/node/index.js
@@ -0,0 +1,14 @@
+function add(){
+    return 6;
+}
+
+function minus(){
+    return 6;
+}
+
+// //Single Function
+// export default add;
+
+
+//Multiple Function
+export { add, minus };
---
+++ React Js/node/server.js
@@ -0,0 +1,8 @@
+import { minus } from "./index.js";
+import { add } from "./index.js";
+
+console.log(add());
+
+
+
+minus()
