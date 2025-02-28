691a18e90e5a3754b9141f0727fd181c500f132f
pradeeprai1990 <pradeep.9997@gmail.com>
2025-02-28 18:19:00 +0530

    Changes

--- NodeJs/mongodb/config/databaseConnection.js
+++ NodeJs/mongodb/config/databaseConnection.js
@@ -3,12 +3,12 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-// const url = 'mongodb://localhost:27017';
-const url = 'mongodb+srv://mhoarhaan14:7DxLr9yl2W8aELwn@cluster0.mmc2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = 'mongodb://localhost:27017';
+// const url = 'mongodb+srv://mhoarhaan14:7DxLr9yl2W8aELwn@cluster0.mmc2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'ecommerce';
+const dbName = 'ecommerce-139';
 
 async function main() {
   // Use connect method to connect to the server
--- NodeJs/mongodb/server.js
+++ NodeJs/mongodb/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
 const server = express(); // To Excutable Function
 const dbConnection = require('./config/databaseConnection.js');
 
@@ -8,38 +10,111 @@ server.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 server.use(express.urlencoded({ extended: true }));
 
+server.use(bodyParser.json());
 
 server.get('', (request, response) => {
     response.send('Server is working fine !');
 })
 
-server.get('/categories', async(request, response) => {
+server.post('/categories/add', async(request, response) => {
 
     const db = await dbConnection();
     const  insertData = await db.collection('categories').insertOne({
-        name : 'Mens'
+        name : request.body.name
     });
 
+    const results = {
+        status: true,
+        message: 'Record Fetch Successfully !!',
+        data: insertData
+    }
 
+    response.send(results);
+})
 
-    // const data = categories;
+server.get('/categories', async(request, response) => {
 
-    // if (data.length > 0) {
+    const db = await dbConnection();
+    const categoryData = await db.collection('categories').find().toArray();
+
+    if (categoryData.length > 0) {
         const results = {
             status: true,
             message: 'Record Fetch Successfully !!',
-            data: insertData
+            data: categoryData
         }
 
         response.send(results);
-    // } else {
-    //     const results = {
-    //         status: false,
-    //         message: 'No Record Found !!',
-    //         data: []
-    //     }
-    //     response.send(results);
-    // }
+    } else {
+        const results = {
+            status: false,
+            message: 'No Record Found !!',
+            data: []
+        }
+        response.send(results);
+    }
+})
+
+server.get('/categories/details', async(request, response) => {
+
+    var id = new mongodb.ObjectId(request.query.id)
+
+
+    const db = await dbConnection();
+    const categoryData = await db.collection('categories').findOne(id);
+
+    if (categoryData) {
+        const results = {
+            status: true,
+            message: 'Record Fetch Successfully !!',
+            data: categoryData
+        }
+
+        response.send(results);
+    } else {
+        const results = {
+            status: false,
+            message: 'No Record Found !!',
+            data: []
+        }
+        response.send(results);
+    }
+})
+
+server.put('/categories/update/:id', async(request, response) => {
+
+    const db = await dbConnection();
+    const  updateData = await db.collection('categories').updateOne({
+        _id : new mongodb.ObjectId(request.params.id)
+    },{
+        $set : {
+            name : request.body.name
+        }
+    });
+
+    const results = {
+        status: true,
+        message: 'Record Updated Successfully !!',
+        data: updateData
+    }
+
+    response.send(results);
+})
+
+server.delete('/categories/delete/:id', async(request, response) => {
+
+    const db = await dbConnection();
+    const  deleteData = await db.collection('categories').deleteOne({
+        _id : new mongodb.ObjectId(request.params.id)
+    });
+
+    const results = {
+        status: true,
+        message: 'Record Deleted Successfully !!',
+        data: ''
+    }
+
+    response.send(results);
 })
 
 server.get('*', (request, response) => {
